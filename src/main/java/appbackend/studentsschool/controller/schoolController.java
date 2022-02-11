e01706ea5762a8c5ca56f50b7b5ca3a8b8c1abde
Alexander Merino <alexmerino67@gmail.com>
2022-02-11 02:53:32 -0500

    Update files

--- src/main/java/appbackend/studentsschool/controller/schoolController.java
+++ src/main/java/appbackend/studentsschool/controller/schoolController.java
@@ -1,8 +1,15 @@
 package appbackend.studentsschool.controller;
 
-@RestController
-@RequestMapping
-public class studentsController {
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-    
+@RestController
+public class schoolController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hola mundo";
+    }
+
 }
