5cd812017843a86fdb5c5b0575a61e0d39a8a4b3
Alexander Merino <alexmerino67@gmail.com>
2022-02-11 01:43:29 -0500

    Add structure project

---
+++ src/main/java/appbackend/studentsschool/StudentsSchoolApplication.java
@@ -0,0 +1,13 @@
+package appbackend.studentsschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appbackend/studentsschool/StudentsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package appbackend.studentsschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
